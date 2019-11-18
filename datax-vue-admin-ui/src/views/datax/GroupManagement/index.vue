c2bbcc18701f080022d569f726c967bb44b040ef
zhouhongfa <zhouhongfa1996@gmail.com>
2019-11-18 11:36:49 +0800

    change: 实现读取列表

--- datax-vue-admin-ui/src/views/datax/GroupManagement/index.vue
+++ datax-vue-admin-ui/src/views/datax/GroupManagement/index.vue
@@ -22,15 +22,15 @@
       fit
       highlight-current-row
     >
-      <el-table-column label="序号" width="110" align="center">
+      <!-- <el-table-column label="序号" width="110" align="center">
         <template slot-scope="scope">{{ scope.row.id }}</template>
-      </el-table-column>
+      </el-table-column> -->
       <el-table-column label="分组名" width="110" align="center">
-        <template slot-scope="scope">{{ scope.row.gn }}
+        <template slot-scope="scope">{{ scope.row.groupname }}
         </template>
       </el-table-column>
       <el-table-column label="类型" width="110" align="center">
-        <template slot-scope="scope">{{ scope.row.tp }}
+        <template slot-scope="scope">{{ scope.row.type }}
         </template>
       </el-table-column>
       <el-table-column label="是否启用" width="110" align="center">
@@ -38,7 +38,7 @@
         </template>
       </el-table-column>
       <el-table-column label="说明" width="110" align="center">
-        <template slot-scope="scope">{{ scope.row.dc }}
+        <template slot-scope="scope">{{ scope.row.comments }}
         </template>
       </el-table-column>
       <el-table-column label="操作" align="center" width="230" class-name="small-padding fixed-width">
@@ -52,14 +52,39 @@
         </template>
       </el-table-column>
     </el-table>
+    <pagination v-show="total>0" :total="total" :page.sync="listQuery.current" :limit.sync="listQuery.size" @pagination="fetchData" />
   </div>
 </template>
 
 <script>
+import * as Api from '@/api/datax-typeGroup'
+import waves from '@/directive/waves' // waves directive
+import { parseTime } from '@/utils'
+import Pagination from '@/components/Pagination' // secondary package based on el-pagination
 export default {
+  name: 'DataxGroupManagement',
+  components: { Pagination },
+  directives: { waves },
+  filters: {
+    statusFilter(status) {
+      const statusMap = {
+        published: 'success',
+        draft: 'gray',
+        deleted: 'danger'
+      }
+      return statusMap[status]
+    }
+  },
   data() {
     return {
       groupName: 1,
+      list: null,
+      listLoading: true,
+      total: 0,
+      listQuery: {
+        current: 1,
+        size: 10
+      },
       options1: [
         { value: 1, text: '金三数据源' },
         { value: 2, text: '数据同步' }
@@ -69,6 +94,114 @@ export default {
         { value: 1, text: '数据源' },
         { value: 2, text: '作业同步' }
       ]
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      this.listLoading = true
+      Api.list(this.listQuery).then(response => {
+        const { records } = response
+        const { total } = response
+        this.total = total
+        this.list = records
+        this.listLoading = false
+      })
+    },
+    resetTemp() {
+      this.temp = {
+        id: undefined,
+        datasourceName: '',
+        datasourceGroup: 'Default',
+        jdbcUsername: '',
+        jdbcPassword: '',
+        jdbcUrl: '',
+        jdbcDriverClass: '',
+        comments: ''
+      }
+    },
+    handleCreate() {
+      this.resetTemp()
+      this.dialogStatus = 'create'
+      this.dialogFormVisible = true
+      this.$nextTick(() => {
+        this.$refs['dataForm'].clearValidate()
+      })
+    },
+    createData() {
+      this.$refs['dataForm'].validate((valid) => {
+        if (valid) {
+          Api.created(this.temp).then(() => {
+            this.fetchData()
+            this.dialogFormVisible = false
+            this.$notify({
+              title: 'Success',
+              message: 'Created Successfully',
+              type: 'success',
+              duration: 2000
+            })
+          })
+        }
+      })
+    },
+    handleUpdate(row) {
+      this.temp = Object.assign({}, row) // copy obj
+      this.dialogStatus = 'update'
+      this.dialogFormVisible = true
+      this.$nextTick(() => {
+        this.$refs['dataForm'].clearValidate()
+      })
+    },
+    updateData() {
+      this.$refs['dataForm'].validate((valid) => {
+        if (valid) {
+          const tempData = Object.assign({}, this.temp)
+          Api.updated(tempData).then(() => {
+            this.fetchData()
+            this.dialogFormVisible = false
+            this.$notify({
+              title: 'Success',
+              message: 'Update Successfully',
+              type: 'success',
+              duration: 2000
+            })
+          })
+        }
+      })
+    },
+    handleDelete(row) {
+      console.log('删除')
+      const idList = []
+      idList.push(row.id)
+      // 拼成 idList=xx
+      // 多个比较麻烦，这里不处理
+      Api.deleted({ idList: row.id }).then(response => {
+        this.fetchData()
+        this.$notify({
+          title: 'Success',
+          message: 'Delete Successfully',
+          type: 'success',
+          duration: 2000
+        })
+      })
+      // const index = this.list.indexOf(row)
+    },
+    handleFetchPv(id) {
+      Api.fetched(id).then(response => {
+        this.pluginData = response
+        this.dialogPvVisible = true
+      })
+    },
+    formatJson(filterVal, jsonData) {
+      return jsonData.map(v => filterVal.map(j => {
+        if (j === 'timestamp') {
+          return parseTime(v[j])
+        } else {
+          return v[j]
+        }
+      }))
     }
   }
 }
